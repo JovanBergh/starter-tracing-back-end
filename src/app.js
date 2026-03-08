519f87dd485143e55c950f912091802757bbe340
Jovan Bergh <jovanbergh17@gmail.com>
2026-03-08 16:24:03 -0600

    Added unique ID's to requests

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const pinoHttp = require("pino-http");
 
 const articlesRouter = require("./articles/articles.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
+
 const app = express();
 
+app.use(pinoHttp());
 app.use(cors());
 app.use(express.json());
 
